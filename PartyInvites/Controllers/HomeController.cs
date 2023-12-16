16f261fadae9c1846633d1a52bcfba7e00a9231c
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-12-17 02:01:54 +0600

    view implemented in progress

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -11,5 +11,31 @@ public class HomeController : Controller
     {
         return View();
     }
+
+    [HttpGet]
+    public ViewResult RsvpForm()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ViewResult RsvpForm(GuestResponse guestResponse)
+    {
+        if (ModelState.IsValid)
+        {
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+        else
+        {
+            return View();
+        }
+        
+    }
+
+    public ViewResult ListResponses()
+    {
+        return View(Repository.Responses.Where(u => u.WillAttend == true));
+    }
 }
 
--- PartyInvites/Models/GuestResponse.cs
+++ PartyInvites/Models/GuestResponse.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace PartyInvites.Models
 {
 	public class GuestResponse
 	{
+		[Required(ErrorMessage = "Please enter your name")]
 		public string? Name { get; set; }
-		public string? Email { get; set; }
-		public string? Phone { get; set; }
-		public bool? WillAttend { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email address")]
+		[EmailAddress]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your phone number")]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "Please specify whether you'll attend")]
+        public bool? WillAttend { get; set; }
 	}
 }
 
