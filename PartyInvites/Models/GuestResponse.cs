bf91791039781038928ebc9fc9c5f619a8e1584a
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2023-12-17 01:01:21 +0600

    GuestResponse model added

---
+++ PartyInvites/Models/GuestResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace PartyInvites.Models
+{
+	public class GuestResponse
+	{
+		public string? Name { get; set; }
+		public string? Email { get; set; }
+		public string? Phone { get; set; }
+		public bool? WillAttend { get; set; }
+	}
+}
+
